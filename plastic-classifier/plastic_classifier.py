7a2cbb277a33e1d301f3158398ab3a486185cf90
Mohammed Talha <sulemanshah432@gmail.com>
2022-08-29 11:39:11 +0530

    update

--- plastic-classifier/plastic_classifier.py
+++ plastic-classifier/plastic_classifier.py
@@ -27,7 +27,7 @@ def set_background(png_file):
         ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-set_background('Programming/eco/background.png')
+set_background('./background.png')
 
 
 def teachable_machine_classification(img, file):
